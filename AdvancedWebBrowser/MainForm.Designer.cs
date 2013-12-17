8a03076f492568c9d55cdf72e85cc6581fa338ef
blingking01 <blingking01@hotmail.co.uk>
2013-12-17 16:43:20 +0000

    Updated browser controls

--- AdvancedWebBrowser/MainForm.Designer.cs
+++ AdvancedWebBrowser/MainForm.Designer.cs
@@ -141,6 +141,8 @@
             // 
             // pnlAddressPanel
             // 
+            this.pnlAddressPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlAddressPanel.BackColor = System.Drawing.SystemColors.Window;
             this.pnlAddressPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlAddressPanel.Controls.Add(this.picPageIcon);
